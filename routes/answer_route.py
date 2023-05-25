b02eebb0c91c8f5345b7fca655fddbcf6a9edffc
tienloctran <loctientran235@gmail.com>
2023-05-25 22:08:07 +0800

    try catch the db processing and create submit_answer_file api

--- routes/answer_route.py
+++ routes/answer_route.py
@@ -6,78 +6,112 @@ from utils.tokenize import to_words
 
 answer_api_router = APIRouter()
 
-
 # get
 # get single answer using 2 params question_no and student_id
 @answer_api_router.get("/api/get_answer/2018/midterm")
 async def get_answer(question_no: str = Query(None), student_id: str = Query(None)):
-
     results = []
     if student_id and question_no:
         collection = db[f"2018MidtermQ{question_no}"]
         filter = {"student_id": student_id}
         results = answer_serializer(collection.find_one(filter))
     else:
-        results = [{"error: cannot find matching data"}]
-    
+        results = [{"error": "cannot find matching data"}]
     return {"status": "ok", "data": results}
 
 # get all
 @answer_api_router.get("/api/get_answers/2018/midterm")
 async def get_answers(question_no: str = Query(None)):
-
     results = []
     if question_no:
         collection = db[f"2018MidtermQ{question_no}"]
         results = answers_serializer(collection.find())
     else:
-        results = [{"error: cannot find matching data"}]
-    
+        results = [{"error": "cannot find matching data"}]
     return {"status": "ok", "data": results}
 
 # post
-# submit studnet's code as string -> to be tokenized and store in database 
+# submit student's code as string -> to be tokenized and stored in the database
 @answer_api_router.post("/api/submit_answer/2018/midterm")
 async def submit_answer(answer: Answer):
-    question_no = answer.question_no
-    python_code = answer.python_code
-    student_id = answer.student_id
-    collection = db[f"2018MidtermQ{question_no}"]
-    
-    tokenized_code = to_words(python_code)    
-    # tokenized_code = python_code
+    try:
+        question_no = answer.question_no
+        python_code = answer.python_code
+        student_id = answer.student_id
+        collection = db[f"2018MidtermQ{question_no}"]
 
-    print(tokenized_code)
-    
-    document = {
-        'student_id': student_id,
-        'tokenized_code': tokenized_code
-    }
-    
-    result = collection.insert_one(document)
-    
-    if result.inserted_id:
-        print("Document inserted successfully.")
-    else:
-        print("Failed to insert the document.")
-   
-    return {"status": "ok"}
- 
+        tokenized_code = to_words(python_code)
+        # tokenized_code = python_code
+
+        print(tokenized_code)
+
+        document = {
+            'student_id': student_id,
+            'tokenized_code': tokenized_code
+        }
+
+        result = collection.insert_one(document)
+
+        if result.inserted_id:
+            print("Document inserted successfully.")
+        else:
+            print("Failed to insert the document.")
+
+        return {"status": "ok"}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+# submit answer in file
+import httpx
+from utils.tokenize import to_words
+
+@answer_api_router.post("/api/submit_answer_file/2018/midterm")
+async def submit_answer_file(answer: Answer):
+    try:
+        question_no = answer.question_no
+        answer_file_url = answer.python_code
+        student_id = answer.student_id
+
+        collection = db[f"2018MidtermQ{question_no}"]
+
+        # Download the answer file using the answer_file_url
+        async with httpx.AsyncClient() as client:
+            response = await client.get(answer_file_url)
+            if response.status_code != 200:
+                return {"status": "error", "message": "Failed to download the answer file."}
+
+            python_code = response.text
+
+        # Tokenize the Python code
+        tokenized_code = to_words(python_code)
+
+        document = {
+            'student_id': student_id,
+            'tokenized_code': tokenized_code
+        }
+
+        result = collection.insert_one(document)
+
+        if result.inserted_id:
+            return {"status": "ok", "message": "Document inserted successfully."}
+        else:
+            return {"status": "error", "message": "Failed to insert the document."}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
 # delete
 @answer_api_router.delete("/api/delete_answer/2018/midterm")
 async def delete_answer(question_no: str = Query(None), student_id: str = Query(None)):
+    try:
+        result = None
 
-    result = None
-
-    if student_id and question_no:
-        collection = db[f"2018MidtermQ{question_no}"]
-        result = collection.delete_one({'student_id': student_id})
-        if result.deleted_count == 1:
-            result = f"Document with studentid {student_id} deleted successfully."
-        else:
-            result = f"No document found with studentid {student_id}."
-    return {"status": "ok", "result": result}  
-
-
-
-    
+        if student_id and question_no:
+            collection = db[f"2018MidtermQ{question_no}"]
+            result = collection.delete_one({'student_id': student_id})
+            if result.deleted_count == 1:
+                result = f"Document with studentid {student_id} deleted successfully."
+            else:
+                result = f"No document found with studentid {student_id}."
+        return {"status": "ok", "result": result}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
