bb983f4a8f8b8530fc2b5a95128db4ada40a7a04
tienloctran <loctientran235@gmail.com>
2023-05-26 10:31:32 +0800

    modify tokenize.py to avoid unwanted generations of (EOF)

--- utils/tokenize.py
+++ utils/tokenize.py
@@ -1,14 +1,12 @@
-
-#tokenize string
 import io
-from tokenize import generate_tokens, TokenInfo
+from tokenize import generate_tokens
+from token import tok_name
 
-def to_words(python_code):
+def to_words(code_string):
     tokenized_code = io.StringIO()
     l = {0: "(EOF)", 4: "(NEWLINE)", 5: "(INDENT)", 6: "(DEDENT)", 59: "(ERROR)"}
-    code_lines = python_code.splitlines()
-    for line in code_lines:
-        tokens = generate_tokens(io.StringIO(line).readline)
-        line_tokens = [l.get(token.type, token.string) if token.type != 3 else token.string.replace("\n", "\\n") for token in tokens if token.type < 60]
-        tokenized_code.write("\n".join(line_tokens) + "\n")
-    return tokenized_code.getvalue()  
+    tokenized_code.write("\n".join((l.get(token.type, token.string) if token.type != 3 else token.string.replace("\n", "\\n") for token in generate_tokens(io.StringIO(code_string).readline) if token.type < 60)))
+    return tokenized_code.getvalue()
+
+
+
